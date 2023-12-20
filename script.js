32b549caa4915dadbf3b93292f2add93b13c1731
Tushar Sharma <145897973+Tushar161616@users.noreply.github.com>
2023-12-20 20:34:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
-	const newd1 = date1.split(-);
-	const newd2 = date2.split(-);
+	// const newd1 = date1.split(-);
+	// const newd2 = date2.split(-);
+	
 	return newd2-newd1;
+	
 };
 
 // Do not change the code below.
