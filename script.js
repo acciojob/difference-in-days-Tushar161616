0fae20807adc68c1de3aacf8cbad4de73a52bb4a
Tushar Sharma <145897973+Tushar161616@users.noreply.github.com>
2025-07-19 20:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
-
-	const date3 = reverse(date1);
-	const date4 = reverse(date2);
-	const diffDays = date4.getDate() - date3.getDate(); 
-// alert(diffDays)
-	return diffDays;
-	
+    const d1 = new Date(date1); // Parse the first date
+    const d2 = new Date(date2); // Parse the second date
+    const diffTime = d2 - d1; // Difference in milliseconds
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); // Convert to days
+    return diffDays; // Return the difference
 };
-function reverse(s) {
-	const ans = s.split('').reverse().join('');
-	return ans ;
-}
+
+
+// 	const ans = s.split('').reverse().join('');
+// 	return ans ;
+// 
 
 // Do not change the code below.
 const dateOne = prompt("Enter Start Date");
